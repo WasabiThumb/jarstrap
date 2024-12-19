f5ec5581a3b55c4e6f7259fef5cd1cd461f6c0bb
WasabiThumb <wasabithumbs@gmail.com>
2024-12-19 13:53:16 -0500

    Fix injection on Windows

--- src/main/java/io/github/wasabithumb/jarstrap/packager/stage/impl/PackagerInjectStage.java
+++ src/main/java/io/github/wasabithumb/jarstrap/packager/stage/impl/PackagerInjectStage.java
@@ -5,11 +5,15 @@ import io.github.wasabithumb.jarstrap.packager.PackagerState;
 import io.github.wasabithumb.jarstrap.packager.error.PackagerException;
 import io.github.wasabithumb.jarstrap.packager.error.PackagerIOException;
 import io.github.wasabithumb.jarstrap.packager.stage.PackagerStage;
+import io.github.wasabithumb.jarstrap.util.io.StreamUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.logging.Level;
 
 public class PackagerInjectStage implements PackagerStage {
 
@@ -41,7 +45,16 @@ public class PackagerInjectStage implements PackagerStage {
                     src.toPath()
             );
         } catch (IOException e) {
-            throw new PackagerIOException("Failed to create symlink to path \"" + src.getAbsolutePath() + "\"", e);
+            packager.logger().log(Level.INFO, "[inject] failed to create symlink, making full copy");
+            try (
+                    FileInputStream fis = new FileInputStream(src);
+                    FileOutputStream fos = new FileOutputStream(dest, false)
+            ) {
+                StreamUtil.pipe(fis, fos);
+            } catch (IOException e2) {
+                e2.addSuppressed(e);
+                throw new PackagerIOException("Failed to move file @ \"" + src.getAbsolutePath() + "\"", e2);
+            }
         }
     }
 
